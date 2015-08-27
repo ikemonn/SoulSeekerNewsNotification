6789671b13bbd9483a6dfd4fcb350b63253fa063
cy-daiki-ikemoto <daiki_ikemoto@cybird.co.jp>
2015-08-27 13:15:22 +0900

    ポップアップにバナーとリンクを表示

--- js/main.js
+++ js/main.js
@@ -33,18 +33,77 @@ chrome.cookies.set({
     xhr.onload = function(e) {
         if (this.status === 200) {
             console.log(this.responseXML);
-            // // ポイント部分のDOMを取得
-            // var evtCountDom = this.responseXML.getElementById("EVTcount");
-            // var numImgDom = evtCountDom.getElementsByTagName("img");
-            // var numCount = numImgDom.length;
-            // var eventPoint = 0;
-            // for (var i = 0; i < numCount; i++) {
-            //     // ポイントを算出
-            //     eventPoint += Math.pow(10, numCount - (i + 1)) * numImgDom[i].getAttribute("alt");
-            // }
-            // chrome.browserAction.setBadgeText({text:String(eventPoint)});
-        } else {
-            console.log("えらー");
+            // 掲載されている「お知らせ」「イベント」のニュース情報
+            var newsData = {};
+
+            // 「お知らせ」のニュース一覧の内容を配列に追加
+            var infoDom = this.responseXML.getElementsByClassName('top');
+            for (var i = 0; i < infoDom.length; i++) {
+                var ancharDom = infoDom[i].getElementsByTagName('a');
+                // 保存するデータ
+                var data = {
+                    type:'link',
+                    url:ancharDom[0].href,
+                    title:ancharDom[0].innerText
+                };
+
+                newsData[ancharDom[0].href] = data;
+            }
+            // 「お知らせ」のニュース一覧の内容を配列に追加
+            var eventDom = this.responseXML.getElementsByClassName('eventlist');
+            for (var i = 0; i < eventDom.length; i++) {
+                var imgDom = eventDom[i].getElementsByTagName('img');
+                for (var j = 0; j < imgDom.length; j++) {
+                    var data = {
+                        type:'img',
+                        url:imgDom[j].src
+                    };
+                    newsData[imgDom[j].src] = data;
+                }
+            }
+
+            var isReSave = false;
+            // 保存していた情報の数と、取得してきた情報の数が違うときは、再保存
+            if (localStorage.length !== newsData.length) {
+                isReSave = true;
+            } else {
+                // 保存していた情報と異なる情報が取得できたときは、再保存
+                for (key in newsData) {
+                    if (localStorage.getItem(key) === null) {
+                        isReSave = true;
+                        break;
+                    }
+                }
+            }
+
+            // localStorageの内容をクリアして再保存
+            if (isReSave === true) {
+                console.log("再保存だよ");
+                localStorage.clear();
+                for (link in newsData) {
+                    localStorage.setItem(link, JSON.stringify(newsData[link]));
+                }
+            }
+
+            // localStorageすべての情報の取得
+            for (var i = 0; i < localStorage.length; i++) {
+                var saveKey = localStorage.key(i);
+                var saveData = JSON.parse(localStorage.getItem(saveKey));
+                console.log(localStorage.getItem(saveKey));
+                console.log(saveData);
+
+                if (saveData.type === 'img') {
+                    $('body').append('<img src="' + saveData.url + '"></img>');
+                } else if (saveData.type === 'link') {
+                    $('body').append('<li><a href="' + saveData.url + '" target="_blank">' + saveData.title + '</a></li>');
+                } else {
+                    console.log('想定していないタイプが取得されました');
+                }
+
+
+            }
+
+
         }
     };
 
